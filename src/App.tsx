80f9281fbe7302b122e140b9ebe66f810c74251c
JR <jose.alonso.programmer@gmail.com>
2022-07-21 13:37:12 +0200

    Copied the Official MagicConnect example. Not working due to a compile error  in the "rpcProvider" line

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./styles.css";
+import { Magic } from "magic-sdk";
+import { ConnectExtension } from "@magic-ext/connect";
+import Web3 from "web3";
 
-function App() {
+const API_KEY = 'pk_live_4FBF6C8E0FD662CA';
+const magic = new Magic("pk_live_73AAE8A5F81B1CF3", {
+  // const magic = new Magic(API_KEY, {
+  network: "rinkeby",
+  locale: "en_US",
+  extensions: [new ConnectExtension()]
+});
+// @ts-ignore
+const web3 = new Web3(magic.rpcProvider);
+
+export default function App() {
+  const [account, setAccount] = useState(null);
+
+  const sendTransaction = async () => {
+    const publicAddress = (await web3.eth.getAccounts())[0];
+    const txnParams = {
+      from: publicAddress,
+      to: publicAddress,
+      value: web3.utils.toWei("0.01", "ether"),
+      gasPrice: web3.utils.toWei("30", "gwei")
+    };
+    web3.eth
+      .sendTransaction(txnParams as any)
+      .on("transactionHash", (hash) => {
+        console.log("the txn hash that was returned to the sdk:", hash);
+      })
+      .then((receipt) => {
+        console.log("the txn receipt that was returned to the sdk:", receipt);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const login = async () => {
+    //   web3.eth
+    //     .getAccounts()
+    //     .then((accounts) => {
+    //       // @ts-ignore
+    //       setAccount(accounts?.[0]);
+    //     })
+    //     .catch((error) => {
+    //       console.log(error);
+    //     });
+  };
+
+  const signMessage = async () => {
+    const publicAddress = (await web3.eth.getAccounts())[0];
+    const signedMessage = await web3.eth.personal
+      .sign("My Message", publicAddress, "")
+      .catch((e) => console.log(e));
+    console.log(signedMessage);
+  };
+
+  const showWallet = () => {
+    magic.connect.showWallet().catch((e) => {
+      console.log(e);
+    });
+  };
+
+  const disconnect = async () => {
+    await magic.connect.disconnect().catch((e) => {
+      console.log(e);
+    });
+    setAccount(null);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h2>Magic Connect</h2>
+      {!account && (
+        <button onClick={login} className="button-row">
+          Sign In
+        </button>
+      )}
+
+      {account && (
+        <>
+          <button onClick={showWallet} className="button-row">
+            Show Wallet
+          </button>
+          <button onClick={sendTransaction} className="button-row">
+            Send Transaction
+          </button>
+          <button onClick={signMessage} className="button-row">
+            Sign Message
+          </button>
+          <button onClick={disconnect} className="button-row">
+            Disconnect
+          </button>
+        </>
+      )}
     </div>
   );
 }
-
-export default App;
